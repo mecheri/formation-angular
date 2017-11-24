a59f94b217e3ec2c69276582cc00123bd58e4402
MECHERI Mehdi <m.mecheri@Viveris.lan>
2017-11-24 19:06:07 +0100

    es7 features

---
+++ 07_ES7/NOTE.md
@@ -0,0 +1,2 @@
+# https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/includes
+# https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Op%C3%A9rateurs/Op%C3%A9rateurs_arithm%C3%A9tiques
---
+++ 07_ES7/main.js
@@ -0,0 +1,19 @@
+//#region Array.prototype.includes()
+let array1 = [1, 2, 3];
+
+console.log(array1.includes(2));
+// expected output: true
+
+var pets = ['cat', 'dog', 'bat'];
+
+console.log(pets.includes('cat'));
+// expected output: true
+
+console.log(pets.includes('at'));
+// expected output: false
+//#endregion
+
+//#region Exponentiation operator
+let result = 3 ** 4;
+console.log(result); //81
+//#endregion
